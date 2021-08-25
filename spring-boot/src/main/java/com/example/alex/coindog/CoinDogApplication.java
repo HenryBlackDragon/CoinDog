7b1c81dea0cbdb12f4a5ac1d22c99255e493be75
Alexander Chaban <alexander.chaban@exposit.com>
2021-08-25 23:28:28 +0300

    first commit

---
+++ spring-boot/src/main/java/com/example/alex/coindog/CoinDogApplication.java
@@ -0,0 +1,13 @@
+package com.example.alex.coindog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinDogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CoinDogApplication.class, args);
+  }
+
+}
